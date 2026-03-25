5da24d3ee2790d29874513186c2cd87192aa4056
Hanyan <hanyan72395@gmail.com>
2026-03-25 13:38:54 +0800

    【layernorm_naive】【完成版本1】增加了调用核函数、核函数编写，并能够正确计算

--- layernorm/layernorm_naive.cu
+++ layernorm/layernorm_naive.cu
@@ -4,8 +4,27 @@
 using namespace std;
 
 
-__global__ void layernorm(float* x, int ){
-    x;
+// 【注】朴素
+__global__ void layernorm(float* x, float* out, size_t B, size_t dim){
+    // x[] = ;
+    size_t thread_idx = blockIdx.x * blockDim.x + threadIdx.x;
+    float sum = 0;
+    float square_sum = 0;
+    // 单次循环获取方差和均值。
+    // 1 累计求和
+    // 2 平方和
+    for(size_t i = 0; i < dim; i++){
+        float val = x[(thread_idx * dim) + i];
+        sum += val / dim;
+        square_sum += val * val / dim;
+    }
+    // float mu = ;
+    float var = square_sum - sum*sum;
+    float inv_std = rsqrtf(var + 1e-5f); // 【重要】rsqrtf求开根再计算
+
+    for(size_t i = 0; i < dim; i++){
+        out[(thread_idx * dim) + i] = (x[(thread_idx * dim) + i] - sum) * inv_std;
+    }
 
 }
 
@@ -37,7 +56,10 @@ __host__ int main(){
     }
     cout << "<==x on host" << endl;
 
+    // 启动kernel（调用global函数）
+    if(B < 256) layernorm<<<1, B>>>(x_d, out_d,  B, dim);
 
+    cudaMemcpy(out_h, out_d, N * sizeof(float), cudaMemcpyDeviceToHost);
 
     for(size_t i = 0; i <  N ; i++){
         if (i % dim == 0) cout << endl;
